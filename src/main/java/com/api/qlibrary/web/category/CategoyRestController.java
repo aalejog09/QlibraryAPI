3fd737f170292aa7588fa3e316c3df89a15e1654
Andres alejo <aalejog09@gmail.com>
2024-02-12 05:50:04 -0400

    update: properties and depuracion

--- src/main/java/com/api/qlibrary/web/category/CategoyRestController.java
+++ src/main/java/com/api/qlibrary/web/category/CategoyRestController.java
@@ -5,24 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.Valid;
-
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.qlibrary.auxiliar.appUser.AppUserCreateDTO;
-import com.api.qlibrary.auxiliar.appUser.AppuserDTO;
-import com.api.qlibrary.auxiliar.book.BookByAuthorDTO;
 import com.api.qlibrary.models.Category;
-import com.api.qlibrary.services.interfaces.IAppuserService;
 import com.api.qlibrary.services.interfaces.ICategoryService;
 
 import lombok.extern.slf4j.Slf4j;
